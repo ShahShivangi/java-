bdc530590d7e672efa764dc774b4a8578d51316f
ShahShivangi <117812039+ShahShivangi@users.noreply.github.com>
2022-11-24 14:12:23 +0530

    print the statement

---
+++ simpleprogram.java
@@ -0,0 +1,11 @@
+ public class Hello
+{
+// define the class
+public static void main(String[] args)
+{
+// this is a main method 
+//we can write the any of the  code in main method
+System.out.println("Hello ! i am useing the method. ");
+
+}
+}
