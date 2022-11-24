0f5704e9e3cdd257e1dc17d3bed79c407c176dd2
ShahShivangi <117812039+ShahShivangi@users.noreply.github.com>
2022-11-24 15:05:38 +0530

    Create coments.java

---
+++ coments.java
@@ -0,0 +1,10 @@
+public class Comments
+{
+  public static void main(String[] args)
+  {
+    System.out.println("This is a comments like single-line comments define the // this symbol");
+    // this is a single-line comments.
+    System.out.println("This is a multi-line comments define in /* .....*/ symbol");
+    
+  }
+}
