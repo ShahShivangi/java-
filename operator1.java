7e37ef2f0e7bad05fc20487d4e4e17056943f5b4
ShahShivangi <117812039+ShahShivangi@users.noreply.github.com>
2022-11-28 00:50:26 +0530

    Arithmetic operators operation we will performs

---
+++ operator1.java
@@ -0,0 +1,15 @@
+ class Main
+ {
+  public static void main(String[] args)
+  {
+    int x = 100 + 50;
+    int y= 100-60;
+    int z=300*2;
+    int u=500/6;
+     System.out.println(x);
+     System.out.println(y);
+     System.out.println(z);
+     System.out.println(u);
+   
+   }
+}
