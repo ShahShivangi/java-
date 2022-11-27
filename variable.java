88bf06ee3dcdc082b365cc7618f938cbb2fe43d7
ShahShivangi <117812039+ShahShivangi@users.noreply.github.com>
2022-11-27 15:42:00 +0530

    how to declare the variable.

---
+++ variable.java
@@ -0,0 +1,13 @@
+class Hello
+{
+  public static void main(String []args)
+  {
+    int x=40;
+    // this is a x is a variable and there are empty and developer put a value and there are excuted.
+    // this is  a int is a data types . 
+    float y=5.55;
+    char y='r';
+    String name="information";
+    // boolean store a string value true or false 
+  }
+}
