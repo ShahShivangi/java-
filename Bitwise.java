514155427cf4960ef68e5abf5454f6efbb1a261e
ShahShivangi <117812039+ShahShivangi@users.noreply.github.com>
2022-11-28 12:39:48 +0530

    bitwise operator  and performs there  operation

---
+++ Bitwise.java
@@ -0,0 +1,13 @@
+class Bitwise  
+{   
+  public static void main(String[] args)   
+  {   
+
+    int x = 9, y = 8;   
+
+    // this is a bitwise operators and there will be used to 
+    // 1001 & 1000 = 1000 = 8  
+
+    System.out.println("x & y = " + (x & y));   
+  }  
+}  
